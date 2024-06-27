fa045e7017dea9244e8b8dfd1192d1e104e8fccd
Bismarck Vanegas <88695142+sirbismarckk@users.noreply.github.com>
2024-06-26 21:58:34 -0600

    Update token.h

--- token.h
+++ token.h
@@ -1,4 +1,4 @@
-const char* ssid = "Nombre de la red";
-const char* password = "";
-const String token = ""; 
-const int64_t IDchat = ;
+/*const char* ssid = "DESKTOP-ADSNPSG 8308";
+const char* password = "12345678";*/
+const String token = "6650969952:AAEujZcQDGNOKEiqxfs4FfnB9cDD2mnl-0w"; 
+const int64_t IDchat = 2136360297;
